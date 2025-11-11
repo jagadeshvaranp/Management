8f9890aaeac64318e75b2351ee7d278d66eb5bcd
jagadeshvaran <jagadeshvaranperumal@gmail.com>
2025-11-11 21:50:25 +0530

    update

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
+
 const User = mongoose.model("User", userSchema);
 
 // Register
@@ -46,14 +47,16 @@ app.post("/api/register", async (req, res) => {
 // Login
 app.post("/api/login", async (req, res) => {
   try {
+    
     const { username, password } = req.body;
     const user = await User.findOne({ username });
+    console.log(user);
     if (!user) return res.status(404).json({ error: "User not found" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: "Invalid password" });
 
-    res.status(200).json({ message: "Login successful!" });
+    res.status(200).json({ user, message: "Login successful!" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
